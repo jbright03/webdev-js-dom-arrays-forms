d072aca753f30d9ec5d1a1e1fc82b3e67fb48b44
Bright <c1025508@hallam.shu.ac.uk>
2021-11-11 10:25:46 +0000

    3

--- Myscript.js
+++ Myscript.js
@@ -1,5 +1,7 @@
 function init()
 {document.getElementById("myHeading").innerHTML = "jamie";
  document.querySelector("nav ul li").setAttribute("class", ".currentpage")
+document.querySelector("nav ul li a").setAttribute("href", "http://www.google.co.uk"
+document.getElementById("mytestevent").onclick = function(){console.info("hi");}
 
 }
